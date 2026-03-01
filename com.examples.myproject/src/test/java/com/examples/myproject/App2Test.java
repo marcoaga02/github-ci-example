3373879860f3f5c62520b8c78d250239ceb9c9fd
Marco Agatensi <marcoagatensi@gmail.com>
2026-03-01 15:16:21 +0100

    failing test

---
+++ com.examples.myproject/src/test/java/com/examples/myproject/App2Test.java
@@ -0,0 +1,22 @@
+package com.examples.myproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class App2Test {
+	
+	private App app;
+	
+	@Before
+	public void setup() {
+		app = new App();
+	}
+	
+	@Test
+	public void testSayHello() {
+		assertEquals("Hallo", app.sayHello());
+	}
+
+}
