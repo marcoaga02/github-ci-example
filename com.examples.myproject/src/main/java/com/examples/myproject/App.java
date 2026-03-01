8351968556e4ab241e0dc6d8e12ee82d6740374f
Marco Agatensi <marcoagatensi@gmail.com>
2026-03-01 16:45:22 +0100

    Add uncovered parts

--- com.examples.myproject/src/main/java/com/examples/myproject/App.java
+++ com.examples.myproject/src/main/java/com/examples/myproject/App.java
@@ -1,6 +1,16 @@
 package com.examples.myproject;
 
 public class App {
+	
+	public String sayHello(String name) {
+		if (name == null) {
+			return "Hello";
+		}
+		
+		return "Hello" + name;
+		
+	}
+	
 	public String sayHello() {
 		return "Hello";
 	}
